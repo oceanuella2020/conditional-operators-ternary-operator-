8e4c27e9d9940dc79c3074351d3d92450e5b4094
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-18 21:19:47 +0100

    Add project files.

---
+++ conditional operators(ternary operator)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace conditional_operators_ternary_operator_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 10;
+            b = (a == 1) ? 20 : 30;
+            Console.WriteLine("value of b is "+b);
+
+            b = (a == 10) ? 20 : 30;
+            Console.WriteLine("value of b is " + b);
+
+            Console.ReadKey();
+        }
+    }
+}
